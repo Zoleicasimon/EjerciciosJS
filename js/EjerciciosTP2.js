9d215e76e36d3372386c2e0b033d3d00f902ac56
zoleicasimon <zoleicas@hotmail.com>
2023-12-02 20:12:32 -0300

    Hasta ejercicio 7

--- js/EjerciciosTP2.js
+++ js/EjerciciosTP2.js
@@ -86,7 +86,7 @@ do {
     
 //4 Realiza un script que pida números hasta que se pulse “cancelar”. Si no es un número deberá indicarse con un «alert» y seguir pidiendo números. Al salir con “cancelar” deberá indicarse la suma total de los números introducidos.
 
-let numero;
+/*let numero;
 let suma=0;
 let respuesta;
 
@@ -109,4 +109,76 @@ do {
 if(!respuesta){
     console.log('La suma total es: ', suma);
     document.write('La suma total es: ', suma);
-};
+};*/
+
+/*5.Realizar una página con un script que calcule el valor de la letra de un número de DNI (Documento nacional de identidad).
+
+El algoritmo para calcular la letra del dni es el siguiente :
+
+El número debe ser entre 0 y 99999999
+Debemos calcular el resto de la división entera entre el número y el número 23.
+Según el resultado, de 0 a 22, le corresponderá una letra de las siguientes:  (T, R, W, A, G, M, Y, F, P, D, X, B, N, J, Z, S, Q, V, H, L, C, K, E) 
+Si lo introducido no es un número deberá indicarse con un alert y volver a preguntar.
+Deberá de repetirse el proceso hasta que el usuario pulse «cancelar».*/
+
+/*let resto;
+let arrayOriginal=['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E']; 
+let respuesta;
+
+do{
+let dni=parseInt(prompt('Ingrese su DNI'));
+
+while(isNaN(dni)===true || dni<=0 || dni>99999999){
+    alert('Ingrese correctamente su dni');
+    dni=parseInt(prompt('Ingrese su DNI'));
+}
+
+if(isNaN(dni)===false && dni>=0 && dni<=99999999){
+  resto=dni%23;
+  console.log(resto, 'Su dni corresponde a la letra', arrayOriginal[resto]);
+}
+respuesta=confirm('¿Desea averiguar otro DNI?')
+
+}while(respuesta===true);
+
+if(!respuesta){
+    console.log('Agradecemos su visita');
+
+}*/
+
+/*6. Realiza un script que escriba una pirámide del 1 al 30 de la siguiente forma :
+1
+22
+333
+4444
+55555
+666666*/
+
+/*let i, rep;
+// BUCLE FOR PARA RECORRER DE 1 A 30//recorre verticalmente
+for (i = 1; i <= 30; i++) { 
+    // BUCLE INTERIOR PARA HACER LAS REPETICIONES//recorre horizontamente
+    for (rep = 0; rep < i; rep++) {
+        // IMPRIMIMOS EL NÚMERO i, i VECES
+        document.write(i);
+    }
+    // IMPRIMIMOS UN SALTO DE LÍNEA
+    document.write("<br>");
+}
+
+//
+
+/*7. 7- Haz un script que escriba una pirámide inversa de los números del 1 al número que indique el usuario (no mayor de 50)  de la siguiente forma : (suponiendo que indica 30)*/
+
+
+let nro=parseInt(prompt('Ingrese un numero'));
+let i,j;
+
+for (i=nro; i>=1; i--){
+    for(j=0; j<i; j++){
+        console.log(nro);
+        document.write(i);
+    }
+
+    document.write('</br>');
+}
